e86380bc90e227da299e39089b133e8b678eed91
TommyNewNew <142307431+TommyNewNew@users.noreply.github.com>
2025-10-21 00:49:17 +0000

    added js for menu

---
+++ script.js
@@ -0,0 +1,18 @@
+function onPageLoad () {
+    // find the nav button and refer to it as "button"
+    const button = document.querySelector('#menu-button')
+    // find the navigation and refer to it as "menu"
+    const menu = document.querySelector('#main')
+    // add a listener for clock on the button and execute function when clicked
+    button.addEventListener("click", function() {
+        // console.log("clicked!")
+        if(menu.classList.contains("open")) {
+            menu.classList.remove("open")
+        }
+        else {
+            menu.classList.add("open")
+        }
+    })
+}
+
+window.addEventListener("load", onPageLoad)
